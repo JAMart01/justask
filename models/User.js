598b74a8beb30504fc1ad35a0caeca9f51af5928
Daniel Carazo <danielcarazo20@gmail.com>
2021-08-25 20:40:31 -0400

    created utils folder and files

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
-// const bcrpyt = require('bcrypt');
+const bcrpyt = require('bcrypt');
 
 class User extends Model {
     // checkPassword(loginPw) {
@@ -36,17 +36,17 @@ User.init(
         }
     },
     {
-        // hooks: {
-        //     async beforeCreate (newUserData) {
-        //         newUserData.password = await bcrpyt.hash(newUserData.password, 10);
-        //         return newUserData;
-        //     },
+        hooks: {
+            async beforeCreate (newUserData) {
+                newUserData.password = await bcrpyt.hash(newUserData.password, 10);
+                return newUserData;
+            },
 
-        //     async beforeUpdate (updatedUserData) {
-        //         updatedUserData.password = await bcrpyt.hash(updatedUserData.password, 10);
-        //         return updatedUserData;
-        //     }
-        // },
+            async beforeUpdate (updatedUserData) {
+                updatedUserData.password = await bcrpyt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
         sequelize,
         timestamps: false,
         freezeTableName: true,
