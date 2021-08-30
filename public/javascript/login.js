5d8813740fed78b4d14993b787d1516c79392e76
Daniel Carazo <danielcarazo20@gmail.com>
2021-08-30 14:29:57 -0400

    combined signup and login file to one, resovled log in issues

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,11 +1,11 @@
 async function loginFormHandler(event) {
     event.preventDefault();
   
-    const username = document.querySelector('#inputUsername').value.trim();
-    const password = document.querySelector('#inputPassword').value.trim();
+    const username = document.getElementById('inputUsername').value.trim();
+    const password = document.getElementById('inputPassword').value.trim();
   
     if (username && password) {
-      const response = await fetch('/api/users', {
+      const response = await fetch('/api/users/login', {
         method: 'post',
         body: JSON.stringify({
           username,
@@ -21,5 +21,33 @@ async function loginFormHandler(event) {
       }
     }
   }
+
+  async function signupFormHandler(event) {
+    event.preventDefault();
+  
+    const username = document.getElementById('inputUsername').value.trim();
+    const email = document.getElementById('inputEmail').value.trim();
+    const password = document.getElementById('inputPassword').value.trim();
+  
+    if (username  && email && password) {
+      const response = await fetch('/api/users/', {
+        method: 'post',
+        body: JSON.stringify({
+          username,
+          email,
+          password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard/');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  }
+
+  document.querySelector('.form-signin').addEventListener('submit', signupFormHandler);
 
   document.querySelector('.form-signin').addEventListener('submit', loginFormHandler);
