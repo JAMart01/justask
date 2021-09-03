5beaea7672c4d30574f2887da7a45bc295003da8
Daniel Carazo <danielcarazo20@gmail.com>
2021-09-03 14:09:13 -0400

    test

--- controller/api/user-routes.js
+++ controller/api/user-routes.js
@@ -84,6 +84,7 @@ router.post('/logout', (req, res) => {
 
       req.session.destroy(() => {
         res.status(204).end();
+        console.log('================== route is working =====================')
       });
     }
     else {
